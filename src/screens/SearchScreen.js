62b654c70e2788a2ef31eba0be3e9b5f366ca172
Vijay Kumar Gattikoppula <gtkvijaykumar@gmail.com>
2024-03-20 12:27:54 +0530

    basic search bar logic

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import {View,StyleSheet,Text} from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
+   const [term, setTerm] = useState('');
+
  return (<View>
+      <SearchBar 
+         data={term} 
+         onChange = { newTerm => setTerm(newTerm)}
+         onSubmit = { () => console.log("submitted")} >
+      </SearchBar>
     <Text>Search Screen</Text>
+    <Text>{term}</Text>
  </View>
  );
 };
