528dfaea0adc8f4ff22734c3a116f408220c9439
Vijay Kumar Gattikoppula <gtkvijaykumar@gmail.com>
2024-06-13 11:37:43 +0530

    last time changes and expo version update

--- src/screens/ResultShowScreen.js
+++ src/screens/ResultShowScreen.js
@@ -1,8 +1,19 @@
-import react from "react";
+import react,{useState,useEffect} from "react";
 import { Text,View, StyleSheet } from "react-native";
+import yelp from "../api/yelp";
 
 const ResultShowScreen = ({navigation}) => {
+    const [result, setResult] = useState(null);
     const id = navigation.getParam('id');
+
+    console.log(result);
+    const getResult = async (id) =>{
+        const response = await yelp.get(`/${id}`);
+        setResult(response.data);
+    };
+    useEffect( () => {
+        getResult(id); 
+    }, []);
     return( 
     <View>
         <Text>This is from Results Show Screen</Text>
